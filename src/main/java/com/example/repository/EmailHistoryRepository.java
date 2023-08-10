7921b185938191c76fc9745633860dcfbdbd57f6
Muhammad Yusuf <myshukurov@gmail.com>
2023-08-10 23:35:03 +0500

    added EmailSenderService

--- src/main/java/com/example/repository/EmailHistoryRepository.java
+++ src/main/java/com/example/repository/EmailHistoryRepository.java
@@ -6,9 +6,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+
 @Repository
 public interface EmailHistoryRepository extends JpaRepository<EmailHistoryEntity, Integer> {
 //    findAllByPriceOrderByCreatedDate/**/
     Page<EmailHistoryEntity> findAllByToEmailOrderByCreatedDate(String price, Pageable pageable);
 
+    Long countAllByEmailAndCreatedDateAfter(String email, LocalDateTime localDateTime);
 }
