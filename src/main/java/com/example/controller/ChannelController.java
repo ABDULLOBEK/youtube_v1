38264fbfc532c2962e6d37ba69c0607535a54a31
Orazgali <77142550+Urazgali@users.noreply.github.com>
2023-08-14 21:08:14 +0500

    Channel full

--- src/main/java/com/example/controller/ChannelController.java
+++ src/main/java/com/example/controller/ChannelController.java
@@ -2,9 +2,10 @@ package com.example.controller;
 
 
 import com.example.dto.ChannelDTO;
-import com.example.dto.PlaylistDTO;
 import com.example.service.ChannelService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 public class ChannelController {
     @Autowired
     private ChannelService channelService;
-
+    @PreAuthorize("hasRole('ROLE_USER')")
     @PostMapping("")
     public ResponseEntity<?> create(@RequestBody ChannelDTO dto){
             return ResponseEntity.ok(channelService.add(dto));
@@ -23,22 +24,55 @@ public class ChannelController {
     @PreAuthorize("hasAnyRole('ROLE_OWNER','ROLE_USER')")
     @PutMapping("/update/{id}")
     public ResponseEntity<?> update(@RequestBody ChannelDTO dto,
-                                    @PathVariable("id") Integer id){
+                                    @PathVariable("id") String id){
         Boolean response =channelService.update(id,dto);
         return ResponseEntity.ok( response);
     }
 
     @PreAuthorize("hasAnyRole('ROLE_OWNER','ROLE_USER')")
     @PutMapping("/update/photo/{id}")
-    public ResponseEntity<?> photoUp(@RequestBody ChannelDTO dto, @PathVariable("id") Integer id){
+    public ResponseEntity<?> photoUp(@RequestBody ChannelDTO dto, @PathVariable("id") String id){
         Boolean response =channelService.PhotoUP(id,dto);
         return ResponseEntity.ok( response);
     }
 
     @PreAuthorize("hasAnyRole('ROLE_OWNER','ROLE_USER')")
     @PutMapping("/update/banner/{id}")
-    public ResponseEntity<?> bannerUp(@RequestBody ChannelDTO dto, @PathVariable("id") Integer id){
+    public ResponseEntity<?> bannerUp(@RequestBody ChannelDTO dto, @PathVariable("id") String id){
         Boolean response =channelService.bannerId(id,dto);
         return ResponseEntity.ok( response);
     }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+//    @GetMapping(value = "/pagination")
+//    public ResponseEntity<?> pagination(@RequestParam(value = "page", defaultValue = "1") int page,
+//                                        @RequestParam(value = "size", defaultValue = "5") int size) {
+//
+//        return ResponseEntity.ok(channelService.pagination(page - 1, size));
+//    }
+    @GetMapping(value = "/pagination")
+    public ResponseEntity<?> pagination(@RequestParam(value = "page", defaultValue = "1") int page,
+                                        @RequestParam(value = "size", defaultValue = "10") int size) {
+        PageImpl<ChannelDTO> response = channelService.pagination(page - 1, size);
+        return ResponseEntity.ok(response);
+
+    }
+    @GetMapping("/search/{id}")
+    public ResponseEntity<?> search(@PathVariable("id") String id){
+        return ResponseEntity.ok(channelService.search(id));
+    }
+
+    @PreAuthorize("hasAnyRole('ROLE_OWNER','ROLE_USER','ROLE_ADMIN')")
+    @PutMapping("/update/status/{id}")
+    public ResponseEntity<?> statusUp(@RequestBody ChannelDTO dto,
+                                      @PathVariable("id") String id){
+        return ResponseEntity.ok(channelService.statusUpdate(id,dto));
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<?> all(){
+        return ResponseEntity.ok(channelService.all());
+    }
+
+
 }
