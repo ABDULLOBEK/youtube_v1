801e4cea06e130cd8c4980f168586d6cc8d9d4d6
Muhammad Yusuf <myshukurov@gmail.com>
2023-08-16 05:54:02 +0500

    comment api begin

---
+++ src/main/java/com/example/service/CommentService.java
@@ -0,0 +1,1 @@
+package com.example.service;import com.example.repository.CommentRepository;import org.springframework.beans.factory.annotation.Autowired;import org.springframework.stereotype.Service;@Servicepublic class CommentService {    @Autowired    private CommentRepository commentRepository;}
