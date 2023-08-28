ad35a771379723b540a8a21c44fab5866590eb60
Muhammad Yusuf <myshukurov@gmail.com>
2023-08-28 19:59:33 +0500

    playlistVideo done

--- src/main/java/com/example/controller/VideoLikeController.java
+++ src/main/java/com/example/controller/VideoLikeController.java
@@ -1,1 +1,1 @@
-package com.example.controller;import org.springframework.web.bind.annotation.RequestMapping;import org.springframework.web.bind.annotation.RestController;@RestController@RequestMapping("")public class VideoLikeController {}
+package com.example.controller;import org.springframework.web.bind.annotation.RequestMapping;import org.springframework.web.bind.annotation.RestController;@RestController@RequestMapping("")public class VideoLikeController {    //}
